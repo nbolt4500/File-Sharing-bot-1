a3ceb0a8c6a1560d5c02c352171ac1b58480f704
nbolt4500 <nbolt4500@gmail.com>
2025-01-01 18:12:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "22350063"))
+  API_HASH = os.environ.get("API_HASH", "21306fd66006fd927c22bbe0af42943c")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7617535781:AAEXIIAX5dOWAFEFMvarnt0kRqN0Vp-kvRw")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "kratosXmania_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002156849345","7135258368"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "https://diskwala.com/")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "6773ea07d87f1f5f7ab21a54")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "7135258368"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://nbolt4500:<db_password>@cluster0.cv9r7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002184025553")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002354196816","-1002222075129"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -30,20 +30,3 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ├🔹 Library: [Pyrogram](https://docs.pyrogram.org)
 │
 ╰──────[ 😎 ]───────────⍟
-"""
-  ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
- 
- I am Super noob Please Support My Hard Work.
-
-[Donate Me](https://t.me/KingVj01)
-"""
-  HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
-
-How to Use Bot & it's Benefits??
-
-📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
-"""
